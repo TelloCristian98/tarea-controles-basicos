3f18dd43375ae082cab90ec65d855f349d0c1139
tellocristian98 <cristian.andres.t98@gmail.com>
2024-06-12 15:46:48 -0500

    Menu listo

---
+++ lib/Layouts/ExerciseOnePage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ExerciseOnePage extends StatelessWidget {
+  const ExerciseOnePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Exercise 1: Customer Balance Calculation'),
+      ),
+      body: Center(
+        child: Text('Exercise 1 content goes here'),
+      ),
+    );
+  }
+}
---
+++ lib/Layouts/ExerciseTwoPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ExerciseTwoPage extends StatelessWidget {
+  const ExerciseTwoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Exercise 2: Weekly Salary Calculation'),
+      ),
+      body: Center(
+        child: Text('Exercise 2 content goes here'),
+      ),
+    );
+  }
+}
